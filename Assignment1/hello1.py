861f111c73eb6bf00514ff85eafa5e0465bb184d
lakshay <lakshaysh19@gmail.com>
2024-08-27 21:21:32 +0530

    Assigment1.ipynb completed on 27-08-2024 -> lakshay sharma

---
+++ Assignment1/hello1.py
@@ -0,0 +1,7 @@
+import platform
+name = "LAKSHAY SHARMA"
+course = "MCA with specialization in AI & ML"
+
+print("Name:", name)
+print("Course:", course)
+print("Python Version:", platform.python_version())
