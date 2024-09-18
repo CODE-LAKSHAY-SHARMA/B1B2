732f45329b85cbcd16c2eddb143b4385800171bc
LAKSHAY SHARMA <122679296+CODE-LAKSHAY-SHARMA@users.noreply.github.com>
2024-09-18 18:58:05 +0530

    learning GUI thinker

---
+++ main.py
@@ -0,0 +1,21 @@
+import tkinter as tk  # GUI
+
+root = tk.Tk()
+root.title("File Organizer")
+root.geometry("400X250")
+
+
+def show_input():
+    user_input = entry.get()
+    print("User Input:", user_input)
+
+
+label = tk.Label(root, text="Laber text content")
+label.pack(pady=10)
+
+
+root.mainloop()
+
+
+# Assignment need to complete
+# Question 1:  Design a file organiser GUI mention your name , Sapid ,course
