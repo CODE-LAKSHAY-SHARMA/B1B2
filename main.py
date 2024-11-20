e750fdf65ca2cbd0029649777b9705c74678b35b
LAKSHAY SHARMA <122679296+CODE-LAKSHAY-SHARMA@users.noreply.github.com>
2024-11-20 23:29:13 +0530

    Server class 20-11-2024

--- main.py
+++ main.py
@@ -1,21 +1,22 @@
-import tkinter as tk  # GUI
+## pip install fastapi uvicorn
+#
+from fastapi import FastAPI, Request
+import uvicorn
+from typing import Union
+import pandas as pd
 
-root = tk.Tk()
-root.title("File Organizer")
-root.geometry("400X250")
+app = FastAPI()
+sapid=[]
+@app.get("/sapid/{item_id}")
+def read_root(item_id:int,q=Union[str,None]=None):
+    sapid.append(item_id)
+    print(f"\n{sapid}\n")
+    data={"present":sapid}
+    df=pd.DataFrame(data)
+    df.to_excel("3_idiotAttendance.xlsx")
 
+    return {"Attendance Marked":item_id}
 
-def show_input():
-    user_input = entry.get()
-    print("User Input:", user_input)
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
 
-
-label = tk.Label(root, text="Laber text content")
-label.pack(pady=10)
-
-
-root.mainloop()
-
-
-# Assignment need to complete
-# Question 1:  Design a file organiser GUI mention your name , Sapid ,course
